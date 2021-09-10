8117eb9ca6b20b376e6648d7f603305e65b22816
neworderofjamie <neworderofjamie@gmail.com>
2021-09-10 14:46:25 +0100

    add horizontal lines for loihi

--- scripts/plot_lstm_vs_genn.py
+++ scripts/plot_lstm_vs_genn.py
@@ -5,18 +5,24 @@ import plot_settings
 # Load data
 data= read_csv("lstm_vs_genn.csv", delimiter=",")
 
-
+# Extract the data for different platforms
+loihi_data=data[data["Device"] == "Loihi"]
 titan_v_data=data[data["Device"] == "Titan V"]
 
-lstm = titan_v_data[(titan_v_data["Model"] == "TF LSTM") & (titan_v_data["Batch size"] > 1)]
-genn = titan_v_data[titan_v_data["Model"] == "GeNN LSNN (256 ALIF)"]
+assert len(loihi_data) == 1
+
+lstm_data = titan_v_data[(titan_v_data["Model"] == "TF LSTM") & (titan_v_data["Batch size"] > 1)]
+genn_data = titan_v_data[titan_v_data["Model"] == "GeNN LSNN (256 ALIF)"]
 
 fig, axes = plt.subplots(2, sharex=True)
-lstm_actor = axes[0].plot(lstm["Batch size"], lstm["Latency per batch [ms]"])[0]
-genn_actor = axes[0].plot(genn["Batch size"], genn["Latency per batch [ms]"])[0]
-axes[1].plot(lstm["Batch size"], lstm["Energy Delay Product (uJs)"])
-axes[1].plot(genn["Batch size"], genn["Energy Delay Product (uJs)"])
 
+lstm_actor = axes[0].plot(lstm_data["Batch size"], lstm_data["Latency per batch [ms]"])[0]
+genn_actor = axes[0].plot(genn_data["Batch size"], genn_data["Latency per batch [ms]"])[0]
+axes[1].plot(lstm_data["Batch size"], lstm_data["Energy Delay Product (uJs)"])
+axes[1].plot(genn_data["Batch size"], genn_data["Energy Delay Product (uJs)"])
+
+axes[0].axhline(loihi_data["Latency per batch [ms]"].iat[0], linestyle="--")
+axes[1].axhline(loihi_data["Energy Delay Product (uJs)"].iat[0], linestyle="--")
 
 axes[1].set_xlabel("Batch size")
 axes[0].set_ylabel("Latency per batch [ms]")
