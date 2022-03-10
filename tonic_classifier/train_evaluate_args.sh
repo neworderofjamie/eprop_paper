8f5a1752927a8c521ca9286cd1edba8a4ca72cad
Jamie Knight <neworderofjamie@gmail.com>
2022-03-10 10:02:55 +0000

    script to train and evaluate model described by arguments text file

---
+++ tonic_classifier/train_evaluate_args.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ARGS=`head -${SLURM_ARRAY_TASK_ID} arguments.txt | tail -1`
+
+python tonic_classifier.py --num-epochs $1 $ARGS
+
+for E in $(seq 0 $1)
+do
+    python tonic_classifier_evaluate.py --trained-epoch $E $ARGS
+done
