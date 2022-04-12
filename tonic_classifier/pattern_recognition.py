69fa25889748b4459477347cbfd0356c6c48a996
neworderofjamie <neworderofjamie@gmail.com>
2022-04-12 11:29:14 +0100

    WIP Deep-R implementation

--- tonic_classifier/pattern_recognition.py
+++ tonic_classifier/pattern_recognition.py
@@ -20,7 +20,169 @@ NUM_RECURRENT = 256
 NUM_OUTPUT = 3
 
 SPARSITY = 0.1
+DEEP_R = True
 
+class DeepR:
+    def __init__(self, sg, optimiser, num_pre, num_post, weight_0):
+        self.sg = sg
+        self.optimiser = optimiser
+        self.num_pre = num_pre
+        self.num_post = num_post
+        self.weight_sd = weight_0 / np.sqrt(self.num_pre)
+        
+        # Zero all bitmask EGPs
+        num_words = int(np.ceil((num_pre * sg.max_row_length) / 32))
+        optimiser.extra_global_params["signChange"].set_values(
+            np.zeros(num_words, dtype=np.uint32))
+
+    
+    def load(self):
+        # Download newly-generated connectivity
+        self.sg.pull_connectivity_from_device()
+    
+        self.unpacked_conn = np.zeros((self.num_pre, self.num_post), dtype=np.uint8)
+        self.unpacked_conn[self.sg.get_sparse_pre_inds(),
+                           self.sg.get_sparse_post_inds()] = 1
+
+                                 
+        # Get views of sign change 
+        self.sign_change_view = self.optimiser.extra_global_params["signChange"].view[:]
+
+        # Get views to state variables
+        self.sg_var_views = {n: v.view for n, v in self.sg.vars.items()}
+        self.optimiser_var_views = {n: v.view for n, v in self.optimiser.vars.items()}
+      
+    def reset(self):
+        # Zero and upload sign change
+        self.sign_change_view[:] = 0
+        self.optimiser.push_extra_global_param_to_device("signChange")
+   
+    def update(self):
+        # Download sign change
+        self.optimiser.pull_extra_global_param_from_device("signChange")
+
+        # Unpack sign change bitmasks
+        sign_change_unpack = np.unpackbits(
+            self.sign_change_view.view(dtype=np.uint8), 
+            count=self.num_pre * self.sg.max_row_length, bitorder="little")
+        
+        # Reshape
+        sign_change_unpack = np.reshape(sign_change_unpack, 
+                                        (self.num_pre, self.sg.max_row_length))
+        
+        # Count dormant synapses
+        total_dormant = np.sum(sign_change_unpack)
+
+        # If no synapses have been made dormant, no need to do anything further
+        if total_dormant == 0:
+            return
+
+        # Download optimiser and synapse group state
+        self.optimiser.pull_state_from_device()
+        self.sg.pull_state_from_device()
+
+        # Loop through rows
+        for i in range(self.num_pre):
+            start_id = self.sg.max_row_length * i
+            end_id = start_id + self.sg.max_row_length
+            inds = self.sg._ind[start_id:end_id]
+            
+            # Select inds in this row which will be made dormant
+            dormant_inds = inds[sign_change_unpack[i] == 1]
+
+            # If there are any
+            num_dormant = len(dormant_inds)
+            if num_dormant > 0:
+                # Decrease row length
+                assert self.sg._row_lengths[i] >= num_dormant
+                self.sg._row_lengths[i] -= num_dormant
+                
+                # Get mask of row entries to keep
+                keep_mask = (sign_change_unpack[i] == 0)
+                slice_length = np.sum(keep_mask)
+                
+                # Clear dormant synapses from unpacked representation
+                self.unpacked_conn[i, dormant_inds] = 0
+                
+                # Remove inactive indices
+                self.sg._ind[start_id:start_id + slice_length] = inds[keep_mask]
+                
+                # Remove inactive synapse group state vars
+                for v in self.sg_var_views.values():
+                    v_row = v[start_id:end_id]
+                    v[start_id:start_id + slice_length] = v_row[keep_mask]
+                
+                # Remove inactive optimiser state vars
+                for v in self.optimiser_var_views.values():
+                    v_row = v[start_id:end_id]
+                    v[start_id:start_id + slice_length] = v_row[keep_mask]
+
+        # Count number of remaining synapses
+        num_synapses = np.sum(self.sg._row_lengths)
+
+        # From this, calculate how many padding synapses there are in data structure
+        num_total_padding_synapses = (self.sg.max_row_length * self.num_pre) - num_synapses
+
+        print(f"{num_synapses} synapses, {num_total_padding_synapses} total padding and {total_dormant} total to activate")
+        # Loop through rows of synaptic matrix
+        num_activations = np.zeros(self.num_pre, dtype=int)
+        for i in range(self.num_pre - 1):
+            num_row_padding_synapses = self.sg.max_row_length - self.sg._row_lengths[i]
+            
+            if num_row_padding_synapses > 0 and num_total_padding_synapses > 0:
+                probability = num_row_padding_synapses / num_total_padding_synapses
+                
+                # Sample number of activations
+                num_row_activations = min(num_row_padding_synapses, 
+                                          np.random.binomial(total_dormant, probability))
+                num_activations[i] = num_row_activations;
+
+                # Update counters
+                total_dormant -= num_row_activations
+                num_total_padding_synapses -= num_row_padding_synapses
+
+        # Put remainder of activations in last row
+        assert total_dormant < (self.sg.max_row_length - self.sg._row_lengths[-1])
+        num_activations[-1] = total_dormant;
+
+        # Loop through rows
+        for i in range(self.num_pre):
+            # If there's anything to activate on this row
+            if num_activations[i] > 0:
+                new_syn_start_ind = (self.sg.max_row_length * i) + self.sg._row_lengths[i]
+                new_syn_end_ind = new_syn_start_ind + num_activations[i]
+                
+                # Get possible inds to chose from
+                possible_inds = np.where(self.unpacked_conn[i] == 0)
+                
+                new_inds = np.random.choice(possible_inds[0], num_activations[i],
+                                            replace=False)
+                # Sample indices
+                self.sg._ind[new_syn_start_ind:new_syn_end_ind] = new_inds
+                
+                # Update connectivity bitmask
+                self.unpacked_conn[i, new_inds] = 1
+
+                # Initialise synapse group state variables
+                for n, v in self.sg_var_views.items():
+                    if n == "g":
+                        v[new_syn_start_ind:new_syn_end_ind] =\
+                            np.random.normal(0.0, self.weight_sd, num_activations[i])
+                    else:
+                        v[new_syn_start_ind:new_syn_end_ind] = 0
+                
+                # Initialise optimiser state variables
+                for v in self.optimiser_var_views.values():
+                    v[new_syn_start_ind:new_syn_end_ind] = 0
+            
+                # Update row length
+                self.sg._row_lengths[i] += num_activations[i]
+
+        # Upload optimiser and synapse group state
+        self.optimiser.push_state_to_device()
+        self.sg.push_state_to_device()
+        self.sg.push_connectivity_to_device()
+        
 #----------------------------------------------------------------------------
 # Neuron models
 #----------------------------------------------------------------------------
@@ -186,20 +348,35 @@ recurrent_recurrent = model.add_synapse_population(
 model.add_custom_update("recurrent_hidden_transpose", "CalculateTranspose", "Transpose",
                         {}, {}, {"variable": genn_model.create_wu_var_ref(recurrent_output, "g", output_recurrent, "g")})
 
-# Add custom updates for updating reduced weights using Adam optimiser
+# Add custom updates for updating weights using Adam optimiser
 input_recurrent_optimiser_var_refs = {"gradient": genn_model.create_wu_var_ref(input_recurrent, "DeltaG"),
                                       "variable": genn_model.create_wu_var_ref(input_recurrent, "g")}
-input_recurrent_optimiser = model.add_custom_update("input_recurrent_optimiser", "GradientLearn", eprop.adam_optimizer_zero_gradient_model,
-                                                    adam_params, adam_vars, input_recurrent_optimiser_var_refs)
 recurrent_output_optimiser_var_refs = {"gradient": genn_model.create_wu_var_ref(recurrent_output, "DeltaG"),
                                        "variable": genn_model.create_wu_var_ref(recurrent_output, "g", output_recurrent, "g")}
+recurrent_recurrent_optimiser_var_refs = {"gradient": genn_model.create_wu_var_ref(recurrent_recurrent, "DeltaG"),
+                                          "variable": genn_model.create_wu_var_ref(recurrent_recurrent, "g")}
+
+
 recurrent_output_optimiser = model.add_custom_update("recurrent_lif_output_optimiser", "GradientLearn", eprop.adam_optimizer_zero_gradient_model,
                                                      adam_params, adam_vars, recurrent_output_optimiser_var_refs)
 
-recurrent_recurrent_optimiser_var_refs = {"gradient": genn_model.create_wu_var_ref(recurrent_recurrent, "DeltaG"),
-                                          "variable": genn_model.create_wu_var_ref(recurrent_recurrent, "g")}
-recurrent_recurrent_optimiser = model.add_custom_update("recurrent_recurrent_alif_optimiser", "GradientLearn", eprop.adam_optimizer_zero_gradient_model,
-                                                        adam_params, adam_vars, recurrent_recurrent_optimiser_var_refs)
+# If we're using Deep-R
+if DEEP_R:
+    input_recurrent_optimiser = model.add_custom_update("input_recurrent_optimiser", "GradientLearn", eprop.adam_optimizer_zero_gradient_sign_track_model,
+                                                        adam_params, adam_vars, input_recurrent_optimiser_var_refs)
+    recurrent_recurrent_optimiser = model.add_custom_update("recurrent_recurrent_optimiser", "GradientLearn", eprop.adam_optimizer_zero_gradient_sign_track_model,
+                                                            adam_params, adam_vars, recurrent_recurrent_optimiser_var_refs)
+    
+    input_recurrent_deep_r = DeepR(input_recurrent, input_recurrent_optimiser, 
+                                   NUM_INPUT, NUM_RECURRENT, WEIGHT_0)
+    recurrent_recurrent_deep_r = DeepR(recurrent_recurrent, recurrent_recurrent_optimiser, 
+                                       NUM_RECURRENT, NUM_RECURRENT, WEIGHT_0)
+else:
+    input_recurrent_optimiser = model.add_custom_update("input_recurrent_optimiser", "GradientLearn", eprop.adam_optimizer_zero_gradient_model,
+                                                        adam_params, adam_vars, input_recurrent_optimiser_var_refs)
+    recurrent_recurrent_optimiser = model.add_custom_update("recurrent_recurrent_optimiser", "GradientLearn", eprop.adam_optimizer_zero_gradient_model,
+                                                            adam_params, adam_vars, recurrent_recurrent_optimiser_var_refs)
+
 
 # Build model
 model.build()
@@ -210,6 +387,10 @@ model.load(num_recording_timesteps=1000)
 # Calculate initial transpose feedback weights
 model.custom_update("CalculateTranspose")
 
+if DEEP_R:
+    input_recurrent_deep_r.load()
+    recurrent_recurrent_deep_r.load()
+    
 # Loop through trials
 output_y_view = output.vars["Y"].view
 output_y_star_view = output.vars["YStar"].view
@@ -258,8 +439,16 @@ for trial in range(1000):
                                            recurrent_recurrent_optimiser])
     adam_step += 1
 
+    if DEEP_R:
+        input_recurrent_deep_r.reset()
+        recurrent_recurrent_deep_r.reset()
+    
     # Now batch is complete, apply gradients
     model.custom_update("GradientLearn")
+    
+    if DEEP_R:
+        input_recurrent_deep_r.update()
+        recurrent_recurrent_deep_r.update()
 
 print(f"Init: {model.init_time}")
 print(f"Init sparse: {model.init_sparse_time}")
